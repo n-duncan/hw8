706957ae5c98eee3ebd225df100ab9f75d861f31
Nick <ncduncan@umail.ucsb.edu>
2017-12-01 10:17:50 -0800

    added starter algorithm

---
+++ hw8.py
@@ -0,0 +1,28 @@
+#
+# Did you receive any help whatsoever from anyone in solving this assignment?
+# [answer]
+#
+# Did you give any help whatsoever to anyone in solving this assignment?
+# [answer]
+#
+def explicit_exponentiation(a, b):
+    # TODO: Find a to the power of b, using an explicit algorithm
+
+
+
+def recursive_exponentiation(a, b):
+   # TODO: Find a to the power of b using a recursive algorithm
+
+
+
+def gcd(a, b):
+    # TODO: find the greatest common denominator of a and b
+
+
+def find_primes(n):
+    # TODO: Find all primes up to n
+
+
+def greatest_prime_factor(n):
+    # TODO: Find the greatest prime factor for n
+
